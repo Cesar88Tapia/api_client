4bfc36cc811cbb5ca0a8ac53ac123f8b120d6f62
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-09-06 14:54:37 -0500

    modified schema

--- schema.sql
+++ schema.sql
@@ -5,13 +5,13 @@ CREATE DATABASE register;
 use register;
 
 
-  CREATE TABLE user(
+  CREATE TABLE users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_name varchar(100) NOT NULL,
     password varchar(50) NOT NULL
   );
 
-  INSERT INTO user(user_name, password) VALUES('carl123', 'Room49'),
-  INSERT INTO user(user_name, password) VALUES('sam321', 'Soul15saver'),
-  INSERT INTO user(user_name, password) VALUES('joe234', 'Trench805'),
-  INSERT INTO user(user_name, password) VALUES('ron789', 'Crust7y'),
+  INSERT INTO users(user_name, password) VALUES('carl123', 'Room49'),
+  INSERT INTO users(user_name, password) VALUES('sam321', 'Soul15saver'),
+  INSERT INTO users(user_name, password) VALUES('joe234', 'Trench805'),
+  INSERT INTO users(user_name, password) VALUES('ron789', 'Crust7y'),
