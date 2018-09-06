7661b89e2171957100c10c4e613ffebf33e17b61
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-09-06 15:13:35 -0500

    modified server routes.js

--- server/routes.js
+++ server/routes.js
@@ -4,19 +4,19 @@ const path = require('path');
 
 
 
-export.getUser = (req, res) => {
+export.getUsers = (req, res) => {
 
-  db.getUsers()
+  db.getUser()
     .then(data => {
       res.status(200).send(data)
     })
     .catch(err => {console.log(err)})
 }
 
-export.postUser = (req, res) => {
+export.postUsers = (req, res) => {
   let userName = req.body.userName;
   let password = req.body.password;
-  db.postUsers(userName, password)
+  db.postUser(userName, password)
   .then(data => {
     res.status(200).send(data)
   })
